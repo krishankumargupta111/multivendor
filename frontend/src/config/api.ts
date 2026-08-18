4a693bc9a648d0b1c64c0e1e01642a532ebce8a9
guptakrishan787 <guptakrishan787@gmail.com>
2026-08-18 11:57:18 +0530

    Initial commit

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 export const api=axios.create({
-    baseURL:'https://multivendor-i0ud.onrender.com',
+
+    baseURL:import.meta.env.VITE_API_URL,
     headers:{
         'Content-Type':'application/json'
     }
