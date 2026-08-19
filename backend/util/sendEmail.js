f799420df8983748e762fd62d9890634258f410e
guptakrishan787 <guptakrishan787@gmail.com>
2026-08-19 21:11:56 +0530

    Initial commit

--- backend/util/sendEmail.js
+++ backend/util/sendEmail.js
@@ -1,20 +1,30 @@
 import nodemailer from 'nodemailer'
-async function sendVerificationEmail(to,subject,body){
-    const transporter=nodemailer.createTransport({
-        service:"gmail",
-        auth:{
-            user:process.env.EMAIL,
-            pass:process.env.PASSWORD
-        }
-    })
-    console.log(process.env.PASSWORD)
-    console.log(process.env.EMAIL)
-    const mailOptions={
-        from:process.env.EMAIL,
-        to,
-        subject,
-        html:body
-    }
-    await transporter.sendMail(mailOptions)
+async function sendVerificationEmail(to, subject, body) {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
+
+  console.log("Checking Gmail SMTP...");
+
+  await transporter.verify();
+
+  console.log("Gmail SMTP connection successful");
+
+  const mailOptions = {
+    from: process.env.EMAIL,
+    to,
+    subject,
+    html: body,
+  };
+
+  console.log("Sending email...");
+
+  const info = await transporter.sendMail(mailOptions);
+
+  console.log("Email sent:", info.messageId);
 }
 export default sendVerificationEmail
