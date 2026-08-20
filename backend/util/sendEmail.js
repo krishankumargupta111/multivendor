8bf221ff0939175071f3143cc1a06a3337750447
guptakrishan787 <guptakrishan787@gmail.com>
2026-08-20 20:18:05 +0530

    Initial commit

--- backend/util/sendEmail.js
+++ backend/util/sendEmail.js
@@ -10,13 +10,13 @@ console.log("PASSWORD exists:", !!process.env.PASSWORD);
 console.log("PASSWORD length:", process.env.PASSWORD?.length);
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true, // Use true for port 465, false for port 587
+     port: 587,
+  secure: false,
     auth: {
       user: process.env.EMAIL,
       pass: process.env.PASSWORD,
     },
-      family: 4,
+     
     connectionTimeout: 10000,
     greetingTimeout: 10000,
     socketTimeout: 10000,
