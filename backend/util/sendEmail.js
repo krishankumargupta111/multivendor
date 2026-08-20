acee9f46c13150240cbbfca7078403a7e967a536
guptakrishan787 <guptakrishan787@gmail.com>
2026-08-20 11:22:01 +0530

    Initial commit

--- backend/util/sendEmail.js
+++ backend/util/sendEmail.js
@@ -1,5 +1,9 @@
 import nodemailer from 'nodemailer'
 async function sendVerificationEmail(to, subject, body) {
+  const dns = require('dns');
+
+
+dns.setDefaultResultOrder('ipv4first');
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
